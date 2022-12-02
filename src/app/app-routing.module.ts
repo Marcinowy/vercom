ac149f5e40e2ecded80dcf42a1fb50936032bb5f
marcin.tomczynski <mbae2t@gmail.com>
2022-12-02 22:05:02 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartComponent } from './chart/components/chart/chart.component';
+import { FormComponent } from './form/components/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/form' },
+  {
+    path: 'chart',
+    component: ChartComponent,
+  },
+  {
+    path: 'form',
+    component: FormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarLink } from '../../interfaces/navbar-link.interface';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NgFor, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+  protected links: NavbarLink[] = [
+    { title: 'Wykres', path: '/chart' },
+    { title: 'Dodanie danych', path: '/form' }
+  ];
+
+}
